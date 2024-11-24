635c7e0313071beedc5313a643aaa636cd88608f
Manoj <manoj.balineni04@gmail.com>
2024-11-24 15:56:46 +0530

    Initial Commit

---
+++ src/main/java/com/example/DockerJenkinsBuild/DockerJenkinsBuildApplication.java
@@ -0,0 +1,13 @@
+package com.example.DockerJenkinsBuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkinsBuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinsBuildApplication.class, args);
+	}
+
+}
